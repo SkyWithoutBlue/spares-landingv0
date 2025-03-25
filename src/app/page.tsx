047b53eab07780271d56573b82787a645c83f637
NikitaChebotov <nikitachebotov@gmail.com>
2025-03-26 01:03:06 +0900

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { Hero } from '@/components/sections/hero';
 import { About } from '@/components/sections/about';
-import { ContactForm } from '@/components/sections/contact-form';
 import { CountriesPartners } from '@/components/sections/countries-partners';
 import { Research } from '@/components/sections/research';
 import { Products } from '@/components/sections/products';
